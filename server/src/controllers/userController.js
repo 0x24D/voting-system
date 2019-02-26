92b1b12500b91fecc7fd454b3601dcc0e77dc097
0x24d <daniel.longfield@tuta.io>
2019-02-26 10:56:32 +0000

    Add circleci config

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -7,7 +7,7 @@ export const hash = (req, res) => {
 export const addAuthenticationAttempt = (req, res) => {
 
 };
-export const restAuthenticationAttempts = (req, res) => {
+export const resetAuthenticationAttempts = (req, res) => {
 
 };
 export const validatePassword = (req, res) => {
