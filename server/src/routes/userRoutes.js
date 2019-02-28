62c4159cb7a01783544ce4b82cf68351ccafc887
6c69616d <44211257+6c69616d@users.noreply.github.com>
2019-02-28 11:11:35 +0000

    Add get user with id functionality

---
+++ server/src/routes/userRoutes.js
@@ -0,0 +1,12 @@
+import {
+  getUserWithId,
+} from '../controllers/userController';
+
+const userRoutes = (app) => {
+// route for actions on a specific user
+app.route('/api/v1/users/:userId')
+// http get using the getUserWithId method
+.get(getUserWithId)
+};
+
+export default userRoutes;
