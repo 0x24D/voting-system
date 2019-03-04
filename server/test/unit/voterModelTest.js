abc17999fb509ce47db1b2838fcc995c951bc526
Jack <jack.woodhouse.97@gmail.com>
2019-03-04 12:04:54 +0000

    added unit tests for voter

---
+++ server/test/unit/voterModelTest.js
@@ -0,0 +1,92 @@
+import { expect } from 'chai';
+
+import Voter from '../../src/models/voterModel';
+
+describe('user', () => {
+  it('should be invalid if username is empty', (done) => {
+    const v = new Voter();
+
+    v.validate((err) => {
+      expect(err.errors.username).to.exist;
+      done();
+    });
+  });
+});
+
+describe('user', () => {
+  it('should be invalid if name is empty', (done) => {
+    const v = new Voter();
+
+    v.validate((err) => {
+      expect(err.errors.name).to.exist;
+      done();
+    });
+  });
+});
+
+describe('user', () => {
+  it('should be invalid if email is empty', (done) => {
+    const v = new Voter();
+
+    v.validate((err) => {
+      expect(err.errors.email).to.exist;
+      done();
+    });
+  });
+});
+
+describe('user', () => {
+  it('should be invalid if password is empty', (done) => {
+    const v = new Voter();
+
+    v.validate((err) => {
+      expect(err.errors.password).to.exist;
+      done();
+    });
+  });
+});
+
+describe('user', () => {
+  it('should be invalid if authentication_attempts is empty', (done) => {
+    const v = new Voter();
+
+    v.validate((err) => {
+      expect(err.errors.authentication_attempts).to.not.exist;
+      done();
+    });
+  });
+});
+
+describe('voter', () => {
+  it('should be invalid if voted is empty', (done) => {
+    const v = new Voter ();
+
+    v.validate((err) => {
+      expect(err.errors.voted).to.not.exist;
+      done();
+    });
+  });
+});
+
+describe('voter', () => {
+  it('should be invalid if date_of_birth is empty', (done) => {
+    const v = new Voter();
+
+    v.validate((err) => {
+      expect(err.errors.date_of_birth).to.exist;
+      done();
+    });
+  });
+});
+
+describe('voter', () => {
+  it('should be invalid if address is empty', (done) => {
+    const v = new Voter();
+
+    v.validate((err) => {
+      expect(err.errors.address).to.not.exist;
+      done();
+    });
+  });
+});
+
