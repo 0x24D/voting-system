d6442e602087d811bd0ff88bc6c4dffa0a1bc07a
6c69616d <44211257+6c69616d@users.noreply.github.com>
2019-03-04 15:08:28 +0000

    Added vote success UI
    Added vote success UI needs to be wired up to server

---
+++ client/src/components/Success.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="Success">
+    <md-card>
+      <div id="text">
+      <h3>Thank you</h3>
+      <h3>Your vote has been cast.</h3>
+      <h3>You can now logout of the system using the button below.</h3>
+
+         <md-button id="logoutBtn" class="md-raised md-accent" @click="submitLogout()">Logout</md-button>
+      </div>
+    </md-card>
+  </div>
+</template>
+<script>
+
+export default {
+    name: 'Success',
+    methods: {
+    submitLogout() {
+      this.$axios
+        // post to the api so logout can be processed 
+        .post("http://localhost:3030/logout")
+        .then(() => {
+          // delete the token from browser storage
+          delete localStorage.token;
+          // navigate to the login screen
+          window.location.href = "/login/";
+        })
+        .catch(error => {
+          alert("Logout Failed");
+        });
+    }
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+#text {
+  padding: 10px;
+  text-align: center;
+}
+.Success {
+  padding: 50px 500px;
+}
+</style>
---
+++ client/src/views/success/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <!--div v-if="authenticated()"-->
+      <Success/>
+    <!--/div>
+    <div v-else>
+      <h1>Please login to view this page</h1>
+    </div-->
+  </div>
+</template>
+
+<script>
+import Success from "../../components/Success.vue";
+
+export default {
+  name: "app",
+  components: {
+    Success,
+  },
+  methods: {
+    // see if the user is authenticated
+    authenticated() {
+      return localStorage.token;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
