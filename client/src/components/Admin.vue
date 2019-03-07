51c10ab47cf7dfcdfb3119d688a756aa88e209ac
0x24d <daniel.longfield@tuta.io>
2019-03-07 13:55:41 +0000

    Add default admin UI
    (cherry picked from commit 6644948233b512d357aae01c6b7d63c6d5780da9)

---
+++ client/src/components/Admin.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="admin">
+    <md-button class="md-raised" @click="goToUsers()">View Users</md-button>
+    <md-button class="md-raised" @click="goToResults()">View Results</md-button>
+    <md-button class="md-raised" @click="goToCandidates()">View Candidates</md-button>
+    <md-button class="md-raised" @click="goToCampaigns()">View Campaigns</md-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Admin',
+  methods: {
+    goToCampaigns() {
+      this.$store.commit('setAdminDisplayMode', false);
+      // this.$store.commit('setCampaignsDisplayMode', true);
+    },
+    goToCandidates() {
+      this.$store.commit('setAdminDisplayMode', false);
+      // this.$store.commit('setCandidatessDisplayMode', true);
+    },
+    goToResults() {
+      this.$store.commit('setAdminDisplayMode', false);
+      this.$store.commit('setResultsDisplayMode', true);
+    },
+    goToUsers() {
+      this.$store.commit('setAdminDisplayMode', false);
+      // this.$store.commit('setUsersDisplayMode', true);
+    },
+  },
+};
+</script>
